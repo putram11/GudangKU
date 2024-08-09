d5b1063c1cd031fa3efea45b27706d2431244b1a
putram11 <putram.works@gmail.com>
2024-08-09 09:38:30 +0700

    Test Deploy

--- server/seeders/20240806131009-seed-goods.js
+++ server/seeders/20240806131009-seed-goods.js
@@ -8,7 +8,7 @@ module.exports = {
       return {
         name: el.name,
         numberOfItems: 300,
-        price: el.price * 10000, 
+        price: el.price, 
         CategoryId: el.CategoryId,
         createdAt: new Date(),
         updatedAt: new Date(),
