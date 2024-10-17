cdfdef01ef0f227e2ce850b2353f6fe0eee75c43
Putra Mahardika <pmahardika@students.hacktiv8.ac.id>
2024-10-17 15:38:11 +0700

    Update authentication.js

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -2,26 +2,36 @@ const { verifyToken } = require("../helpers/jsonwebtoken");
 const { User } = require("../models");
 
 const Authentication = async (req, res, next) => {
-  
   try {
+    // Mengambil header authorization
     const authHeader = req.headers["authorization"];
-   
-    if (!authHeader) throw { name: "Unauthorized" };
 
+    // Memastikan header authorization ada
+    if (!authHeader) {
+      throw { name: "Unauthorized", message: "Authorization header missing" };
+    }
+
+    // Memisahkan token dari header
     const token = authHeader.split(" ")[1];
-    if (!token) throw { name: "Unauthorized" };
-  
+    if (!token) {
+      throw { name: "Unauthorized", message: "Token missing" };
+    }
 
+    // Memverifikasi token
     const decoded = verifyToken(token);
     const user = await User.findByPk(decoded.id);
 
-    if (!user) throw { name: "Unauthorized" };
+    // Memastikan user ada
+    if (!user) {
+      throw { name: "Unauthorized", message: "User not found" };
+    }
 
+    // Menyimpan informasi user di request
     req.user = user;
-    next();
+    next(); // Melanjutkan ke middleware berikutnya
   } catch (error) {
     console.log(error);
-    next(error);
+    next(error); // Meneruskan error ke middleware penanganan error
   }
 };
 
